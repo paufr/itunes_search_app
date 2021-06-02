9a2b7cf4905223ac9586c5d657b80ab60e8190b8
paufr <paufr94@gmail.com>
2021-06-02 05:40:51 +0200

    docker file updated

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,15 @@
-FROM node:10 AS ui-build
+FROM node:14 AS ui-build
 WORKDIR /usr/src/app
 COPY frontend/ ./frontend/
 RUN cd frontend && npm install && npm run build
 
-FROM node:10 AS server-build
+FROM node:14 AS server-build
 WORKDIR /root/
 COPY --from=ui-build /usr/src/app/frontend/dist ./frontend/dist
-COPY api/package*.json ./api/
+COPY backend/package*.json ./api/
 RUN cd api && npm install
-COPY api/app.js ./api/
+COPY backend/ ./api/
 
-EXPOSE 4000
+EXPOSE 8080
 
 CMD ["node", "./api/app.js"]
--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const history = require('connect-history-api-fallback');
@@ -22,7 +23,7 @@ const artistsRouter = require('./routes/artistsRouter')(artistsController);
 app.use('/api', artistsRouter);
 
 app.use(history());
-app.use('/', express.static('../frontend/dist', {index: 'index.html' }));
+app.use('/', express.static(path.resolve(__dirname, '../frontend/dist'), {index: 'index.html' }));
 
 app.server = app.listen(port, () => {
   console.log(`Running on port ${port}`);
